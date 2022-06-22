2a3edfee68c1270384e06b208c95c40d1ff19efd
charlesndalton <charlesndalton@gmail.com>
2022-06-22 15:20:50 -0700

    test: complete migrating tests to foundry

--- src/test/StrategyRevoke.t.sol
+++ src/test/StrategyRevoke.t.sol
@@ -1,55 +1,87 @@
-// // SPDX-License-Identifier: AGPL-3.0
-// pragma solidity ^0.8.12;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.12;
 
-// import {StrategyFixture} from "./utils/StrategyFixture.sol";
+import {StrategyFixture} from "./utils/StrategyFixture.sol";
+import {IVault} from "../interfaces/Yearn/Vault.sol";
+import {Strategy} from "../Strategy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@yearnvaults/contracts/yToken.sol";
 
-// contract StrategyRevokeTest is StrategyFixture {
-//     function setUp() public override {
-//         super.setUp();
-//     }
+contract StrategyRevokeTest is StrategyFixture {
+    function setUp() public override {
+        super.setUp();
+    }
 
-//     function testRevokeStrategyFromVault(uint256 _amount) public {
-//         vm.assume(_amount > minFuzzAmt && _amount < maxFuzzAmt);
-//         deal(address(want), user, _amount);
+    function testRevokeStrategyFromVault(uint256 _fuzzAmount) public {
+        vm.assume(_fuzzAmount > minFuzzAmt && _fuzzAmount < maxFuzzAmt);
+        for(uint8 i = 0; i < assetFixtures.length; ++i) {
+            AssetFixture memory _assetFixture = assetFixtures[i];
+            IVault vault = _assetFixture.vault;
+            Strategy strategy = _assetFixture.strategy;
+            IERC20 want = _assetFixture.want;
 
-//         // Deposit to the vault and harvest
-//         vm.prank(user);
-//         want.approve(address(vault), _amount);
-//         vm.prank(user);
-//         vault.deposit(_amount);
-//         skip(1);
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+            uint256 _amount = _fuzzAmount;
+            uint8 _wantDecimals = IERC20Metadata(address(want)).decimals();
+            if (_wantDecimals != 18) {
+                uint256 _decimalDifference = 18 - _wantDecimals;
 
-//         // In order to pass these tests, you will need to implement prepareReturn.
-//         // TODO: uncomment the following lines.
-//         // vm.prank(gov);
-//         // vault.revokeStrategy(address(strategy));
-//         // skip(1);
-//         // vm.prank(strategist);
-//         // strategy.harvest();
-//         // assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
-//     }
+                _amount = _amount / (10 ** _decimalDifference);
+            }
 
-//     function testRevokeStrategyFromStrategy(uint256 _amount) public {
-//         vm.assume(_amount > minFuzzAmt && _amount < maxFuzzAmt);
-//         deal(address(want), user, _amount);
+            deal(address(want), user, _amount);
 
-//         vm.prank(user);
-//         want.approve(address(vault), _amount);
-//         vm.prank(user);
-//         vault.deposit(_amount);
-//         skip(1);
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+            // Deposit to the vault and harvest
+            vm.prank(user);
+            want.approve(address(vault), _amount);
+            vm.prank(user);
+            vault.deposit(_amount);
+            skip(1);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
 
-//         vm.prank(gov);
-//         strategy.setEmergencyExit();
-//         skip(1);
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
-//     }
-// }
+            vm.prank(gov);
+            vault.revokeStrategy(address(strategy));
+            skip(1);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
+        }
+    }
+
+    function testRevokeStrategyFromStrategy(uint256 _fuzzAmount) public {
+        vm.assume(_fuzzAmount > minFuzzAmt && _fuzzAmount < maxFuzzAmt);
+        for(uint8 i = 0; i < assetFixtures.length; ++i) {
+            AssetFixture memory _assetFixture = assetFixtures[i];
+            IVault vault = _assetFixture.vault;
+            Strategy strategy = _assetFixture.strategy;
+            IERC20 want = _assetFixture.want;
+
+            uint256 _amount = _fuzzAmount;
+            uint8 _wantDecimals = IERC20Metadata(address(want)).decimals();
+            if (_wantDecimals != 18) {
+                uint256 _decimalDifference = 18 - _wantDecimals;
+
+                _amount = _amount / (10 ** _decimalDifference);
+            }
+
+            deal(address(want), user, _amount);
+
+            vm.prank(user);
+            want.approve(address(vault), _amount);
+            vm.prank(user);
+            vault.deposit(_amount);
+            skip(1);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+
+            vm.prank(gov);
+            strategy.setEmergencyExit();
+            skip(1);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
+        }
+    }
+}
--- src/test/StrategyShutdown.t.sol
+++ src/test/StrategyShutdown.t.sol
@@ -1,81 +1,113 @@
-// // SPDX-License-Identifier: AGPL-3.0
-// pragma solidity ^0.8.12;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.12;
 
-// import {StrategyFixture} from "./utils/StrategyFixture.sol";
+import {StrategyFixture} from "./utils/StrategyFixture.sol";
+import {IVault} from "../interfaces/Yearn/Vault.sol";
+import {Strategy} from "../Strategy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@yearnvaults/contracts/yToken.sol";
+contract StrategyShutdownTest is StrategyFixture {
+    function setUp() public override {
+        super.setUp();
+    }
 
-// contract StrategyShutdownTest is StrategyFixture {
-//     function setUp() public override {
-//         super.setUp();
-//     }
+    function testVaultShutdownCanWithdraw(uint256 _fuzzAmount) public {
+        vm.assume(_fuzzAmount > minFuzzAmt && _fuzzAmount < maxFuzzAmt);
+        for(uint8 i = 0; i < assetFixtures.length; ++i) {
+            AssetFixture memory _assetFixture = assetFixtures[i];
+            IVault vault = _assetFixture.vault;
+            Strategy strategy = _assetFixture.strategy;
+            IERC20 want = _assetFixture.want;
 
-//     function testVaultShutdownCanWithdraw(uint256 _amount) public {
-//         vm.assume(_amount > minFuzzAmt && _amount < maxFuzzAmt);
-//         deal(address(want), user, _amount);
+            uint256 _amount = _fuzzAmount;
+            uint8 _wantDecimals = IERC20Metadata(address(want)).decimals();
+            if (_wantDecimals != 18) {
+                uint256 _decimalDifference = 18 - _wantDecimals;
 
-//         // Deposit to the vault
-//         vm.prank(user);
-//         want.approve(address(vault), _amount);
-//         vm.prank(user);
-//         vault.deposit(_amount);
-//         assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
+                _amount = _amount / (10 ** _decimalDifference);
+            }
 
-//         uint256 bal = want.balanceOf(user);
-//         if (bal > 0) {
-//             vm.prank(user);
-//             want.transfer(address(0), bal);
-//         }
+            deal(address(want), user, _amount);
 
-//         // Harvest 1: Send funds through the strategy
-//         skip(7 hours);
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+            // Deposit to the vault
+            vm.prank(user);
+            want.approve(address(vault), _amount);
+            vm.prank(user);
+            vault.deposit(_amount);
+            assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
 
-//         // Set Emergency
-//         vm.prank(gov);
-//         vault.setEmergencyShutdown(true);
+            uint256 bal = want.balanceOf(user);
+            if (bal > 0) {
+                vm.prank(user);
+                want.transfer(address(0), bal);
+            }
 
-//         // Withdraw (does it work, do you get what you expect)
-//         vm.prank(user);
-//         vault.withdraw();
+            // Harvest 1: Send funds through the strategy
+            skip(7 hours);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
 
-//         assertRelApproxEq(want.balanceOf(user), _amount, DELTA);
-//     }
+            // Set Emergency
+            vm.prank(gov);
+            vault.setEmergencyShutdown(true);
 
-//     function testBasicShutdown(uint256 _amount) public {
-//         vm.assume(_amount > minFuzzAmt && _amount < maxFuzzAmt);
-//         deal(address(want), user, _amount);
+            // Withdraw (does it work, do you get what you expect)
+            vm.prank(user);
+            vault.withdraw();
 
-//         // Deposit to the vault
-//         vm.prank(user);
-//         want.approve(address(vault), _amount);
-//         vm.prank(user);
-//         vault.deposit(_amount);
-//         assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
+            assertRelApproxEq(want.balanceOf(user), _amount, DELTA);
+        }
+    }
 
-//         // Harvest 1: Send funds through the strategy
-//         skip(1 days);
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+    function testBasicShutdown(uint256 _fuzzAmount) public {
+        vm.assume(_fuzzAmount > minFuzzAmt && _fuzzAmount < maxFuzzAmt);
+        for(uint8 i = 0; i < assetFixtures.length; ++i) {
+            AssetFixture memory _assetFixture = assetFixtures[i];
+            IVault vault = _assetFixture.vault;
+            Strategy strategy = _assetFixture.strategy;
+            IERC20 want = _assetFixture.want;
 
-//         // Earn interest
-//         skip(1 days);
+            uint256 _amount = _fuzzAmount;
+            uint8 _wantDecimals = IERC20Metadata(address(want)).decimals();
+            if (_wantDecimals != 18) {
+                uint256 _decimalDifference = 18 - _wantDecimals;
 
-//         // Harvest 2: Realize profit
-//         vm.prank(strategist);
-//         strategy.harvest();
-//         skip(6 hours);
+                _amount = _amount / (10 ** _decimalDifference);
+            }
 
-//         // Set emergency
-//         vm.prank(strategist);
-//         strategy.setEmergencyExit();
+            deal(address(want), user, _amount);
 
-//         vm.prank(strategist);
-//         strategy.harvest(); // Remove funds from strategy
+            // Deposit to the vault
+            vm.prank(user);
+            want.approve(address(vault), _amount);
+            vm.prank(user);
+            vault.deposit(_amount);
+            assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA);
 
-//         assertEq(want.balanceOf(address(strategy)), 0);
-//         assertGe(want.balanceOf(address(vault)), _amount); // The vault has all funds
-//         // NOTE: May want to tweak this based on potential loss during migration
-//     }
-// }
+            // Harvest 1: Send funds through the strategy
+            skip(1 days);
+            vm.prank(strategist);
+            strategy.harvest();
+            assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
+
+            // Earn interest
+            skip(1 days);
+
+            // Harvest 2: Realize profit
+            vm.prank(strategist);
+            strategy.harvest();
+            skip(6 hours);
+
+            // Set emergency
+            vm.prank(strategist);
+            strategy.setEmergencyExit();
+
+            vm.prank(strategist);
+            strategy.harvest(); // Remove funds from strategy
+
+            assertEq(want.balanceOf(address(strategy)), 0);
+            assertRelApproxEq(want.balanceOf(address(vault)), _amount, DELTA); // The vault has all funds
+        }
+    }
+}
